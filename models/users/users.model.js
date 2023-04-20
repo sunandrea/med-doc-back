b64c46b17d4b3991b121c52d0dc7de0e4074af86
SunAndrea <andriishtohryn@gmail.com>
2023-04-20 17:21:30 +0300

    Update users.model.js

--- models/users/users.model.js
+++ models/users/users.model.js
@@ -1,1 +1,50 @@
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+    },
+    number: {
+      type: String,
+      required: [true, "Number is required"],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+
+    role: {
+      type: String,
+      required: [true, "Role is required"],
+      enum: ["Patient", "Doctor"],
+    },
+    rating: {
+      type: Number,
+    },
+    experience: {
+      institution: { type: String },
+      description: { type: String },
+      startDate: { type: String },
+      endDate: { type: String },
+    },
+    token: { type: String },
+
+    avatarURL: {
+      type: String,
+    },
+    verify: {
+      type: Boolean,
+      default: false,
+    },
+    verificationCode: {
+      type: String,
+      default: "",
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
