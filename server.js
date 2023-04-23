6f286df8f09739dfc85d8dbcd42d30079cdea7bc
SunAndrea <andriishtohryn@gmail.com>
2023-04-23 14:31:51 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const DB_URI = process.env.DB_URI;
 const startServer = async () => {
   try {
     await mongoose.connect(DB_URI);
-    app.listen(3000, () => console.log("Database connection successful"));
+    app.listen(process.env.PORT || 3000, () =>
+      console.log("Database connection successful")
+    );
   } catch (error) {
     console.log(error);
     process.exit(1);
