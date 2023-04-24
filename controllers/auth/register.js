312b25642567698699b43ed11235034f59238dd9
Ruslan Formanchuk <ruslan12032@gmail.com>
2023-04-24 20:02:09 +0300

    add /info/update/image

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,5 +1,5 @@
 const bcrypt = require("bcryptjs");
-const gravatar = require("gravatar");
+// const gravatar = require("gravatar");
 const { uuid } = require("uuidv4");
 const { Conflict } = require("http-errors");
 const { User } = require("../../models/users.model");
@@ -10,7 +10,8 @@ const register = async (req, res) => {
   if (user) {
     throw new Conflict(`Number in use`);
   }
-  const avatarURL = gravatar.url(name);
+  const avatarURL =
+    "https://res.cloudinary.com/dzigcvgtu/image/upload/v1682354181/avatars/ohsunxdb9nh2tuzo5cky.png";
   const hashPassword = await bcrypt.hash(password, bcrypt.genSaltSync(10));
   const verificationToken = uuid();
   const result = await User.create({
