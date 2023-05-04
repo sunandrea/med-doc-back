a158cc5242ae9ca32c8902d11c79dbc74c43fb5d
SunAndrea <andriishtohryn@gmail.com>
2023-05-04 23:02:49 +0300

    Update errorFilter.middleware.js

--- middlewares/errorFilter.middleware.js
+++ middlewares/errorFilter.middleware.js
@@ -1,6 +1,6 @@
 const errorFilter = (error, req, res, next) => {
-  // const { status = 500 } = error;
-  res.status(500).json(error);
+  const { status = 500, message = "Something went wrong" } = error;
+  res.status(status).json({ message });
 };
 
 module.exports = errorFilter;
